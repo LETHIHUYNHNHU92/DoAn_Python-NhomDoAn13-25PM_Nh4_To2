d8d2f51a694ba581d44c9bd7545780ef837efb87
Huynh Nhu <huynhnhu6022@gamil.com>
2025-11-08 23:23:28 +0700

    Initialize main application window with Tkinter

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from tkcalendar import DateEntry
+
+root = tk.Tk()
+
+root.title("Ứng dụng Quản lý Học sinh (Đồ án Python)")
+
+root.geometry("800x600")
+
+root.resizable(False, False)
+
+root.mainloop() 
